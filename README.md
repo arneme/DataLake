d2a948ae90e24ba1944080ca35a55d2872b4590f
Arne Munch-Ellingsen <arneme@arneme.net>
2019-10-01 21:02:32 +0200

    First working version

--- README.md
+++ README.md
@@ -9,14 +9,50 @@ This README file includes a summary of the project, how to run the Python script
 4. [Notes](#notes)
 
 ## <a name="summary"></a>Summary of the Data lake project
-The purpose of this project is to provide Sparkify the necessary tool to retrieve vital, business oriented, information about their service. Their current main focus is to be able to, easily and fast, to retrieve information about which songs users are listening to. They do have information about this stored in logfiles already but it is not easy to search or aggregate data from these logfiles. In the Data Lake assignment we have been asked to transform data from the logfiles together with meta data about songs (also stored in files) into a database design that will allow Sparkify to meet their business need related to finding out which songs their users are listening to.
+The purpose of this project is to provide Sparkify the necessary tool to retrieve vital, business oriented, information about their service. Their current main focus is to be able to, easily and fast, to retrieve information about which songs users are listening to. They do have information about this stored in logfiles already but it is not easy to search or aggregate data from these logfiles. In the Data Lake assignment we have been asked to transform data from the logfiles together with meta data about songs (also stored in files) into a business oriented (star) database design that will allow Sparkify to meet their business need related to for example finding out which songs their users are listening to and when.
 
-In the Data Lake project we will extract the logfiles and song data files into Spark and also use Spark to transform the files and store Spark parquet files that follows the business oriented star schema. I.e. we will create artist, song, user, time dimension tables and a songplay fact table stored as Spark parquet files.
+In the Data Lake project we will read the logfiles and song data files into Spark and also use Spark to transform the files and store Spark parquet files that follows the business oriented star schema. I.e. we will create artist, song, user, time dimension tables and a songplay fact table stored as Spark parquet files.
 
 ## <a name="run"></a>How to run the scripts
 
-### Setup a Spark Cluster using your AWS Console
-The python script is a script designed to run on a Spark cluster and it is therefore necessary to setup the cluster before the _etl.py_ script can be runned.
+### Setup a Spark Cluster using AWS Console
+The python script _etl.py_ is a script designed to run on a Spark cluster and it is therefore necessary to setup the cluster before the _etl.py_ script can be run. The next paragraphs describes the steps used in order to setup a cluster for this project and how to run the script on the cluster.
+
+#### Create keys
+To login to the master node of your Spark cluster (this is where you will run the script) it is necessary to generate a keypair for ssh. The private part of the key will have to be stored on the machine you use to login from and the public key will be on the master Spark node. The next paragtraph describes how to do this.
+
+Login to your AWS account and select the EC2 service. Select *Key Pairs* from *Network and Security* in the right hand side menu. Click on the  *Create Key Pair* button and give it an appropriate name (the private part of the key will be automatically downloaded to your local computer. Store it in a safe place).
+
+#### Setup Spark Cluster
+The easiest way to setup a Spark cluster is to use the (mostly) automated Elastic Map Reduce (EMR) service. The steps we have used is as follows:
+
+1. Login to your AWS account and select the EMR service
+2. Click on the *Create Cluster* button
+3. Give the cluster a name and maker sure that *Launch Mode*: *Cluster* is selected
+4. Select hardware configuration *m5.xlarge* and software configuration release *emr-5.27.0* and application *Spark*.
+5. Select the number of nodes in your cluster
+5. In the *Security and Access* configuration make sure to select the keypair you crerated in the previous section
+6. Scroll down and click the final *Create Cluster* button
+
+You can of course use other types of EC2 instances for your cluster if you wish. For this project it is sufficient to have one node (since the amount of data is not really large) but you can use more if you wish :-)
+
+After a while your Spark cluster should be up and running and you are ready to run your pySpark PYTHON script (or notebook) using this cluster.
 
 ### Run etl.py standalone
+To transfer the python script to your Spark master node, first transfer it to a pre created bucket (data-lake-sparkify) and then use _aws s3 cp s3://data-lake-sparkify/etl.py ._ to copy it to the EMR master node (after logging in to the master node using ssh).
+
+Login to your cluster using ssh naming the private key using the -i option (change the path to where you have stored the pem file). Note: You will have to add a rule to the firewall for the master node that allows inbound traffic from your IP address first (click on the *Security groups for* master in the cluster overview to do this).
+
+
+_ssh -i ~/.ssh/spark-cluster.pem hadoop@ec2-35-160-245-xxx.us-west-2.compute.amazonaws.com_
+
+When logged in, you can run the python script like this (after you have copied it to the master node):
+
+_spark\_submit etl.py_
+
+## <a name="notes"></a>Notes
+The _etl.py_ script assumes that a bucket called s3://data-lake-sparkify exists (this is where it will store the parquet files). Before you run the script you should create a bucket for the parquet files and change the name in the _etl.py_ file accordingly.
+
+Since the etl.py script is uploaded to the master node there is no use for the dl.cfg file and it has not been included in the github repo as part of the submission.
+
 
--- etl.py
+++ etl.py
@@ -1,93 +1,163 @@
-import configparser
 from datetime import datetime
-import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
-
-
-config = configparser.ConfigParser()
-config.read('dl.cfg')
-
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
-
+from pyspark.sql.functions import hour, dayofmonth, weekofyear, month, year, dayofweek 
+import pyspark.sql.types as t
 
 def create_spark_session():
+    """
+    Create Spark session
+
+    """
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .appName("Data Lake Udacity project") \
         .getOrCreate()
     return spark
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+    Process song data, i.e. read all song data files into Spark and 
+    transform into OLAP like song and artist (dimensions) tables.
+    Store the song and artist tables into Spark parquet files.
+
+    Parameters
+    ----------
+    spark : 
+        The Spark session to use
+    input_data : string
+        The path to the input files (in S3)
+    output_data : string
+        The path to where the generated parquet files will be stored
+    """
     # get filepath to song data file
-    song_data = 
+    song_data_path = input_data + 'song_data' + '/*/*/*/*.json'
     
     # read song data file
-    df = 
+    song_df = spark.read.json(song_data_path)
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = song_df.select('song_id', 'artist_id', \
+                                 'title', 'year', 'duration') \
+                        .dropDuplicates()
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.mode("overwrite") \
+                    .parquet('s3://data-lake-sparkify/song_table')
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = song_df.select('artist_id', 'artist_name', \
+                                'artist_location', 'artist_latitude', \
+                                'artist_longitude') \
+                            .dropDuplicates()
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.mode("overwrite") \
+                      .parquet('s3://data-lake-sparkify/artist_table')
 
 
 def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
-    log_data =
+    """
+    Process log data, i.e. read the log files into Spark and 
+    transform into OLAP like user, time (dimensions) tables
+    and songplay (fact) table. Store the fact and dimension tables
+    into Spark parquet files.
 
-    # read log data file
-    df = 
+    Parameters
+    ----------
+    spark : 
+        The Spark session to use
+    input_data : string
+        The path to the input log files (in S3)
+    output_data : string
+        The path to where the generated parquet files will be stored
+    """
+    # get filepath to log data file
+    song_data_path = input_data + 'song_data' + '/*/*/*/*.json'
+    log_data = input_data + 'log_data' + '/*/*/*.json'
+
+    event_df = spark.read.json(log_data)
     
     # filter by actions for song plays
-    df = 
+    event_df = event_df.filter(event_df.page == 'NextSong')
 
     # extract columns for users table    
-    artists_table = 
+    user_table = event_df.select('userId', 'firstName', 'lastName', \
+                                 'gender', 'level', ) \
+                         .dropDuplicates()
     
     # write users table to parquet files
-    artists_table
+    user_table.write.mode('overwrite').parquet(output_data + '/user_table')
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
-    
-    # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    def get_ts (ts):
+        return datetime.fromtimestamp(ts / 1000.0)
+
+    get_datetime = udf(lambda z: get_ts(z), t.TimestampType())
+
+    datetime_df = event_df.select('ts').dropDuplicates() \
+                          .withColumn('datetime', get_datetime('ts'))
     
     # extract columns to create time table
-    time_table = 
+    time_table = datetime_df.select(col('ts').alias('starttime'),
+                   hour('datetime').alias('hour'), 
+                   dayofmonth('datetime').alias('day'), 
+                   weekofyear('datetime').alias('week'),
+                   month('datetime').alias('month'),
+                   year('datetime').alias('year'),
+                   dayofweek('datetime').alias('weekday'))
     
     # write time table to parquet files partitioned by year and month
-    time_table
+    time_table.write.mode("overwrite") \
+                    .partitionBy("year", "month") \
+                    .parquet('s3://data-lake-sparkify/time_table')
 
-    # read in song data to use for songplays table
-    song_df = 
+    # read in song data again to use for creating songplays table
+    # Would probably have been a good idea to keep the dataframe
+    # created in process_song_data, but the template seems to
+    # dictate that it should be read again...
 
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    song_df = spark.read.json(song_data_path)
 
-    # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    # extract columns from joined song and log datasets to create
+    # songplays table 
+    songplays_table = event_df.join(song_df, \
+                                    event_df.artist == song_df.artist_name) \
+                              .select('ts', 'userId', 'song_id', \
+                                      'artist_id', 'level', 'sessionId',\
+                                      'location', 'userAgent') \
+                              .dropDuplicates()
+
+    # write songplays table to parquet files
+    # partitioned by year and month (which does not make sense since it is
+    # not in table, I will use song_id and artist_id instead).
+
+    songplays_table.write.mode("overwrite") \
+                        .partitionBy("song_id", "artist_id") \
+                        .parquet('s3://data-lake-sparkify/song_table')
 
 
 def main():
+    """
+    Main program
+    Will call the process_song_data and process_log_data functions
+    to create all the OLAP oriented dimensions and fact tables and
+    store them as parquet files for later use.
+
+    Stops the spark session when done.
+    """
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = ""
+    input_data = "s3n://udacity-dend/"
+    # I have created a bucket named data-lake-sparkify
+    # under my AWS account where all parquet files will be stored.
+    output_data = "s3n://data-lake-sparkify/"
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
+
+    spark.stop()
 
 
 if __name__ == "__main__":
